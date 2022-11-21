41608f347a7985d59a61f29ab8c9b887d6fc288a
twl <twlics.10@nycu.edu.tw>
2022-11-21 19:16:39 +0800

    openmp

--- src/kmeans.cc
+++ src/kmeans.cc
@@ -36,6 +36,51 @@ void scaling(double* p1, double scale, long dim){
   }
 }
 
+double squareDistanceSIMD(double *v1, double *v2, long s, long e){
+
+  //mutiple accumulators
+  __m256d sumBuffer1 = _mm256_set1_pd(0);
+  __m256d sumBuffer2 = _mm256_set1_pd(0);
+  __m256d sumBuffer3 = _mm256_set1_pd(0);
+  __m256d sumBuffer4 = _mm256_set1_pd(0);
+  size_t step = 4;// 4 double = 64 bytes.
+  size_t i = s;
+  for(;i+4*step < e; i+=4*step){ 
+  //for(;i+2*step <= N; i+=2*step){ 
+  //for(;i+step <= N; i+=step){ 
+    __m256d p1 = _mm256_load_pd(v1+i);
+    __m256d p2 = _mm256_load_pd(v2+i);
+    __m256d sub1 = _mm256_sub_pd(p1, p2);
+    __m256d p3 = _mm256_load_pd(v1+i+4);
+    __m256d p4 = _mm256_load_pd(v2+i+4);
+    __m256d sub2 = _mm256_sub_pd(p3, p4);
+    __m256d p5 = _mm256_load_pd(v1+i+8);
+    __m256d p6 = _mm256_load_pd(v2+i+8);
+    __m256d sub3 = _mm256_sub_pd(p5, p6);
+    __m256d p7 = _mm256_load_pd(v1+i+12);
+    __m256d p8 = _mm256_load_pd(v2+i+12);
+    __m256d sub4 = _mm256_sub_pd(p7, p8);
+    sumBuffer1 = _mm256_fmadd_pd(sub1, sub1, sumBuffer1);
+    sumBuffer2 = _mm256_fmadd_pd(sub2, sub2, sumBuffer2);
+    sumBuffer3 = _mm256_fmadd_pd(sub3, sub3, sumBuffer3);
+    sumBuffer4 = _mm256_fmadd_pd(sub4, sub4, sumBuffer4);
+  }
+
+  sumBuffer1 = _mm256_add_pd(sumBuffer1, sumBuffer2);
+  sumBuffer2 = _mm256_add_pd(sumBuffer3, sumBuffer4);
+  sumBuffer1 = _mm256_add_pd(sumBuffer1, sumBuffer2);
+
+  double result[step];
+  _mm256_store_pd(result, sumBuffer1);
+  double remain = 0;
+  for(;i < e;i++){
+    remain += (v1[i]-v2[i])*(v1[i]-v2[i]);
+  }
+  double sum = 0;
+  for(int j = 0; j < step; j++)
+    sum+=result[j];
+  return sum+ remain;
+}
 
 
 //simd
@@ -173,7 +218,7 @@ void kmeans::naiveFit(dataSetPtr& data){
   while(difference > _tol && iters<_maxIter){
 
 
-    #pragma omp parallel for collapse(2) num_threads(2)
+    #pragma omp parallel for collapse(2) num_threads(16)
     for(long i = 0; i < numOfData; i++){ 
       for(int c = 0; c < k;++c){ 
           squareDistances[i][c] = squareDistance(data[i], raw(clusters[c]), 0, dim);
@@ -257,7 +302,6 @@ void kmeans::SIMDFit(dataSetPtr& ds){
   std::vector<alignedVector>clusters = init(data);
   double difference = std::numeric_limits<double>::max();
   int iters = 0;
-  std::vector<std::vector<double>>squareDistances(numOfData, std::vector<double>(k,0));
   if(_verbose){
     std::cout<<"Iterations:"<<iters<<"\n";
     for(long c = 0; c < k; ++c){
@@ -269,34 +313,63 @@ void kmeans::SIMDFit(dataSetPtr& ds){
     std::cout<<"inertia:"<<_inertia<<"\n";
 
   }
+  std::vector<std::vector<double>>squareDistances(numOfData, std::vector<double>(k,0));
+
+  const int threadNum = 16;
+
   while(difference > _tol && iters<_maxIter){
 
-
-    #pragma omp parallel for collapse(2) num_threads(2)
+    #pragma omp parallel for  num_threads(threadNum)
     for(long i = 0; i < numOfData; i++){ 
-      for(int c = 0; c < k;++c){ 
-          squareDistances[i][c] = squareDistanceSIMD(raw(data[i]), raw(clusters[c]), dim);
-          //squareDistances[i][c] = squareDistance(raw(data[i]), raw(clusters[c]), 0, dim);
-      }
+        for(int c = 0; c < k;++c){ 
+            //squareDistances[i][c] += squareDistanceSIMD(raw(data[i]), raw(clusters[c]), d, std::min(d+tile,dim));
+            //squareDistances[i][c] = squareDistance(raw(data[i]), raw(clusters[c]), 0, dim);
+            squareDistances[i][c] = squareDistanceSIMD(raw(data[i]), raw(clusters[c]), 0, dim);
+        }
     }
 
     std::vector<alignedVector>nextClusters(k, alignedVector(dim,0));
     std::vector<long>clusterSize(k,0);
     _inertia = 0;
     //assign data to closet cluster.
-    for(long i = 0; i < numOfData; i++){
-      long closet=0;
-      double minimumDist = std::numeric_limits<double>::max();
-      for(long c = 0; c < k; c++){
-        if(squareDistances[i][c] < minimumDist){
-          minimumDist = squareDistances[i][c];
-          closet = c;
+    
+    struct collector{
+      std::vector<alignedVector>cSum;
+      std::vector<long>cSize;
+    };
+
+    collector collectors[threadNum];
+    for(int i = 0; i < threadNum; i++){
+      collectors[i].cSum.resize(k, alignedVector(dim,0));
+      collectors[i].cSize.resize(k,0);
+    }
+
+    #pragma omp parallel num_threads(threadNum)
+    {
+      int id = omp_get_thread_num();
+      for(long i = id; i < numOfData; i+=threadNum){
+        long closet=0;
+        double minimumDist = std::numeric_limits<double>::max();
+        for(long c = 0; c < k; c++){
+          if(squareDistances[i][c] < minimumDist){
+            minimumDist = squareDistances[i][c];
+            closet = c;
+          }
         }
+        addVecSIMD(raw(collectors[id].cSum[closet]), raw(data[i]), dim);
+        collectors[id].cSize[closet]++;
+        #pragma omp atomic
+        _inertia += minimumDist;
       }
-      addVecSIMD(raw(nextClusters[closet]), raw(data[i]), dim);
-      //addVec(raw(nextClusters[closet]), raw(data[i]), dim);
-      clusterSize[closet]++;
-      _inertia += minimumDist;
+    }
+
+
+    //combine
+    for(long i = 0; i < threadNum; i++){
+      for(long c = 0; c < k; c++){
+        addVecSIMD(raw(nextClusters[c]), raw(collectors[i].cSum[c]), dim);
+        clusterSize[c] += collectors[i].cSize[c];
+      }
     }
 
 
--- test/mnistTest.cc
+++ test/mnistTest.cc
@@ -3,6 +3,7 @@
 #include <loadMnist.hpp>
 #include <kmeans.hpp>
 #include <chrono>
+#include <cassert>
 using namespace std;
 int main(int argc,char*argv[]){
     dataset mnist = load_mnist("./mnist/train-images-idx3-ubyte","./mnist/train-labels-idx1-ubyte"); 
@@ -14,11 +15,15 @@ int main(int argc,char*argv[]){
     kms.fit(mnistPtr);
     auto end = chrono::steady_clock::now();
     std::cout<<"non-simd"<<chrono::duration_cast<chrono::milliseconds>(end - start).count()<<"ms\n";
+    std::cout<<"inertia:"<<kms._inertia<<"\n";
 
     kmeans kmsSimd(10,300,1e-4,false, true);
     start = chrono::steady_clock::now();
     kmsSimd.fit(mnistPtr);
     end = chrono::steady_clock::now();
     std::cout<<"simd"<<chrono::duration_cast<chrono::milliseconds>(end - start).count()<<"ms\n";
+    std::cout<<"inertia:"<<kmsSimd._inertia<<"\n";
+
+    assert(abs(kmsSimd._inertia - kms._inertia)/ kmsSimd._inertia < 1e-10);
     return 0;
 }

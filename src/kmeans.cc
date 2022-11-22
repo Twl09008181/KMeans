193ad08bab9fe9cff12bd4d121c4b28baffc76e3
twl09008181 <twl0985314726@gmail.com>
2022-11-22 01:33:51 -0800

    modify step

--- src/kmeans.cc
+++ src/kmeans.cc
@@ -69,42 +69,42 @@ double squareDistanceSIMD(double *v1, double *v2, long s, long e){
   size_t step = 4;// 4 double = 64 bytes.
   size_t i = s;
   for(;i+4*step < e; i+=4*step){ 
-  //for(;i+2*step <= N; i+=2*step){ 
-  //for(;i+step <= N; i+=step){ 
     __m256d p1 = _mm256_load_pd(v1+i);
     __m256d p2 = _mm256_load_pd(v2+i);
     __m256d sub1 = _mm256_sub_pd(p1, p2);
-    __m256d p3 = _mm256_load_pd(v1+i+4);
-    __m256d p4 = _mm256_load_pd(v2+i+4);
+    __m256d p3 = _mm256_load_pd(v1+i+step);
+    __m256d p4 = _mm256_load_pd(v2+i+step);
     __m256d sub2 = _mm256_sub_pd(p3, p4);
-    __m256d p5 = _mm256_load_pd(v1+i+8);
-    __m256d p6 = _mm256_load_pd(v2+i+8);
+    __m256d p5 = _mm256_load_pd(v1+i+2*step);
+    __m256d p6 = _mm256_load_pd(v2+i+2*step);
     __m256d sub3 = _mm256_sub_pd(p5, p6);
-    __m256d p7 = _mm256_load_pd(v1+i+12);
-    __m256d p8 = _mm256_load_pd(v2+i+12);
+    __m256d p7 = _mm256_load_pd(v1+i+3*step);
+    __m256d p8 = _mm256_load_pd(v2+i+3*step);
     __m256d sub4 = _mm256_sub_pd(p7, p8);
     sumBuffer1 = _mm256_fmadd_pd(sub1, sub1, sumBuffer1);
     sumBuffer2 = _mm256_fmadd_pd(sub2, sub2, sumBuffer2);
     sumBuffer3 = _mm256_fmadd_pd(sub3, sub3, sumBuffer3);
     sumBuffer4 = _mm256_fmadd_pd(sub4, sub4, sumBuffer4);
   }
-
   sumBuffer1 = _mm256_add_pd(sumBuffer1, sumBuffer2);
   sumBuffer2 = _mm256_add_pd(sumBuffer3, sumBuffer4);
   sumBuffer1 = _mm256_add_pd(sumBuffer1, sumBuffer2);
-
   double result[step];
   _mm256_store_pd(result, sumBuffer1);
   double sum = 0;
   for(int j = 0; j < step; j++)
     sum+=result[j];
-
   double remain = 0;
   for(;i < e;i++){
     remain += (v1[i]-v2[i])*(v1[i]-v2[i]);
   }
   return sum+ remain;
 }
+
+
+
+
+
 
 double squareDistance(double *v1, double *v2, long start, long end, bool simd=false){
   bool isAlignment = ((intptr_t(v1) & 0x1f)==0) && ((intptr_t(v2) & 0x1f)==0);

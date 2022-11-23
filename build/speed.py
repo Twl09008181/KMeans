6959a6a3e1c83125ea40efb51d02c910327fa3f1
twl <twlics.10@nycu.edu.tw>
2022-11-23 22:00:50 +0800

    test: add speed test for different engine and different threadNum
    compare speed with skelearn/simd/non-simd version with threadNum 1,2,4,8,16
    check result in speed.txt

--- build/speed.py
+++ build/speed.py
@@ -37,33 +37,43 @@ def digits_setup_mnist32():
     return images, initCluster, 300
 
 
-def test_digits_mnist(engine, dataType):
-    if dataType==32:
-        x, initCluster, max_iter = digits_setup_mnist32()
-    elif dataType == 64:
-        x, initCluster, max_iter = digits_setup_mnist64()
-    else:
-        print("not support type")
+def test_digits_mnist(engine, dataType, x, initCluster, threadNum):
 
     if(engine == "sklearn"):
-        kmeans = KMeans(n_clusters=10, max_iter=max_iter, init=initCluster)
+        kmeans = KMeans(n_clusters=10, max_iter=max_iter, init=initCluster, n_init=1)
     
     elif(engine =="simd"):
         if dataType == 32:
-            kmeans = myKMeans.kmeans32(10, initCluster, max_iter, 1e-4 ,False, True, 8)
+            kmeans = myKMeans.kmeans32(10, initCluster, max_iter, 1e-4 ,False, True, threadNum)
         else:
-            kmeans = myKMeans.kmeans64(10, initCluster, max_iter, 1e-4 ,False, True, 8)
+            kmeans = myKMeans.kmeans64(10, initCluster, max_iter, 1e-4 ,False, True, threadNum)
     else: 
         if dataType == 32:
-            kmeans = myKMeans.kmeans32(10, initCluster, max_iter, 1e-4 ,False, False, 8)
+            kmeans = myKMeans.kmeans32(10, initCluster, max_iter, 1e-4 ,False, False, threadNum)
         else:
-            kmeans = myKMeans.kmeans64(10, initCluster, max_iter, 1e-4 ,False, False, 8)
+            kmeans = myKMeans.kmeans64(10, initCluster, max_iter, 1e-4 ,False, False, threadNum)
 
     s = perf_counter()
     kmeans.fit(x)
     e = perf_counter()
-    print(e-s,"s")
-    
+    return e-s
+
+def warn(*arg, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+engine, dataType, repeat, threadNum = sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])
+if dataType==32:
+    x, initCluster, max_iter = digits_setup_mnist32()
+elif dataType == 64:
+    x, initCluster, max_iter = digits_setup_mnist64()
+else:
+    print("not support type")
 
 
-test_digits_mnist(sys.argv[1], int(sys.argv[2]))
+#repeat
+t = 0
+for i in range(repeat):
+    t+=test_digits_mnist(engine, dataType, x, initCluster, threadNum)
+print(engine,dataType,t/repeat,"s (avg)")
